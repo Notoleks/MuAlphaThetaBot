b2bc92474130ad172fe2534440a6aadfbd311765
Notoleks <yuan8935@gmail.com>
2025-06-28 10:35:02 -0400

    basic puppeteer

--- scraper.js
+++ scraper.js
@@ -5,18 +5,18 @@ import puppeteer from 'puppeteer';
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
-  await page.goto('https://famat.org/results/');
+  await page.goto('https://famat.org/results/static/reports/Full_Precalculus_Indv_Cypress%20Bay%20Regional%20March%202025.html');
 
   await page.setViewport({width: 1080, height: 1024});
 
-  await page.type('h3', 'Regional');
+  await page.type('tr', 'Coral Springs Charter');
 
-  await page.waitForSelector('h3');
+  await page.waitForSelector('tr');
 
   const textSelector = await page.waitForSelector(
-    'text/Regional',
+    'text/Coral Springs Charter',
   );
-  
+
   const competition = await textSelector?.evaluate(el => el.textContent);
 
   console.log(competition);
