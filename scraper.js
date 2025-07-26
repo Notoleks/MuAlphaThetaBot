cc16161879140536d30328089ee0341fdb7856d1
Notoleks <yuan8935@gmail.com>
2025-07-26 10:18:47 -0400

    click on comp

--- scraper.js
+++ scraper.js
@@ -5,26 +5,36 @@ import puppeteer from 'puppeteer';
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
-  await page.goto('https://famat.org/results/static/reports/Full_Precalculus_Indv_Cypress%20Bay%20Regional%20March%202025.html');
+  
 
-  // await page.waitForSelector('a', '2025-State-Convention');
-  // await page.click('a', '2025-State-Convention');
-
-  // await page.screenshot({
-  //   path: 'hn.png',
-  // });
+  // await page.goto('https://famat.org/results/static/reports/Full_Precalculus_Indv_Cypress%20Bay%20Regional%20March%202025.html');
+  await page.goto('https://famat.org/results/');
 
   await page.setViewport({width: 1080, height: 1024});
 
-  await page.type('tr', 'Coral Springs Charter');
+  await page.type('a', 'Cypress Bay')
+
+  const competitionSelector = 'text/Cypress Bay';
+  await page.waitForSelector(competitionSelector);
+  await page.click(competitionSelector);
+
+
+  const categorySelector = 'text/Results';
+  await page.waitForSelector(categorySelector);
+  await page.click(categorySelector);
+
+
 
   await page.waitForSelector('tr');
+
+  await page.type('tr', 'Coral Springs Charter');
+
+
 
   const textSelector = await page.waitForSelector(
     'text/Coral Springs Charter',
   );
 
-  // const results = await textSelector?.evaluate(el => el.textContent);
 
   const results = await textSelector?.evaluate(() => {
   return Array.from(document.querySelectorAll('tr')).map(el => el.textContent);
@@ -48,3 +58,4 @@ import puppeteer from 'puppeteer';
 
   await browser.close();
 })();
+
