c84ac9a57d40d2391caff02b6a7081357c2e53c9
Notoleks <yuan8935@gmail.com>
2025-08-23 11:54:25 -0400

    return competition category

--- scraper.js
+++ scraper.js
@@ -29,7 +29,9 @@ function getUserInput(prompt) {
   // });
   // console.log(textInput);
 
-  const compLink = await getUserInput('Enter comp results link');
+  const compLink = await getUserInput('Enter comp results link ');
+  // const schoolName = 'Coral Springs Charter';
+  const schoolName = await getUserInput('Enter school name ');
 
   // await page.goto('https://famat.org/results/static/reports/Full_Precalculus_Indv_Cypress%20Bay%20Regional%20March%202025.html');
   await page.goto('https://famat.org/results/');
@@ -41,14 +43,18 @@ function getUserInput(prompt) {
   // const competitionSelector = 'text/Cypress Bay';
   // await page.waitForSelector(competitionSelector);
   // await page.click(competitionSelector);
-  const schoolName = 'Coral Springs Charter'
 
+//*[@id="results-container"]/div[2]/h4
+//*[@id="results-container"]/div[3]/h4
+// /html/body/div[1]/div[2]/h4
   for (let i = 0; i < 12; i++){
     divNum += 1;
     await page.goto(compLink);
-    const categorySelector = `::-p-xpath(//*[@id="results-container"]/div[${divNum}]/div/a[1])`;
-    await page.waitForSelector(categorySelector, { visible: true });
-    await page.click(categorySelector);
+
+    const categorySelector = `::-p-xpath(//*[@id="results-container"]/div[${divNum}]/h4)`
+    const resultLink = `::-p-xpath(//*[@id="results-container"]/div[${divNum}]/div/a[1])`;
+    await page.waitForSelector(resultLink, { visible: true });
+    await page.click(resultLink);
 
     console.log(categorySelector);
 
